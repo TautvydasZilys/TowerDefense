fbbe359530e05080ff2c5beb6ebafe3af1e7c7d6
Sunius <suniusx@gmail.com>
2014-07-07 20:28:44 +0300

    Added scripts.

---
+++ Assets/Scripts/Behaviours/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using TowerDefense.DataStructures;
+
+namespace TowerDefense.Behaviours
+{
+    public class Enemy : MonoBehaviour
+    {
+        private List<Direction> m_Directions;
+
+        void Start()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Behaviours/Target.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using TowerDefense.DataStructures;
+
+namespace TowerDefense.Behaviours
+{
+    public class Target : MonoBehaviour
+    {
+        public static Target Instance;
+        public Vector2Int Position { get { return m_Position; } }
+
+        private Vector2Int m_Position;
+
+        public Target()
+        {
+            Instance = this;
+        }
+
+        void Start()
+        {
+            var worldPos = transform.position;
+            m_Position = new Vector2Int(worldPos.x, worldPos.z);
+        }
+    }
+}
