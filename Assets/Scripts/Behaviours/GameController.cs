dd7acadea5798f4f7102df83f1c2b264c7f71c88
Sunius <suniusx@gmail.com>
2014-07-24 21:28:52 +0300

    Added camera controls.

--- Assets/Scripts/Behaviours/GameController.cs
+++ Assets/Scripts/Behaviours/GameController.cs
@@ -12,16 +12,32 @@ namespace TowerDefense.Behaviours
         public float kSpawnInterval = 1.0f;
         public Vector3 kSpawnPoint;
         public GameObject kEnemyPrefab;
+        
+        public float kCameraMinimumHeight = 1.0f;
+        public float kCameraMaximumHeight = 50.0f;
+        public float kCameraScrollSpeed = 1.0f;
+        public float kCameraZoomSpeed = 1.2f;
+        public int kCameraScrollBorderThickness = 10;
 
         #endregion
 
         private List<Enemy> m_Enemies = new List<Enemy>();
-        
+        private Rigidbody m_Rigidbody;
+
         private void Start()
         {
             StartCoroutine(SpawnEnemies());
             StartCoroutine(EnemyUpdate());
+
+            m_Rigidbody = rigidbody;
         }
+
+        private void Update()
+        {
+            UpdateCamera();
+        }
+
+        #region Enemy control
 
         private IEnumerator SpawnEnemies()
         {
@@ -50,5 +66,68 @@ namespace TowerDefense.Behaviours
                 }
             }
         }
+
+        #endregion
+
+        #region Camera control
+
+        private void UpdateCamera()
+        {
+            var mousePosition = Input.mousePosition;
+            var screenWidth = Screen.width;
+            var screenHeight = Screen.height;
+            var deltaTime = Time.deltaTime;
+
+            var newPosition = m_Rigidbody.position;
+            bool shouldMove = false;
+            
+            if (mousePosition.x < kCameraScrollBorderThickness)
+            {
+                newPosition += new Vector3(-deltaTime * kCameraScrollSpeed * newPosition.y, 0.0f, 0.0f);
+                shouldMove = true;
+            }
+            else if (mousePosition.x > screenWidth - kCameraScrollBorderThickness)
+            {
+                newPosition += new Vector3(deltaTime * kCameraScrollSpeed * newPosition.y, 0.0f, 0.0f);
+                shouldMove = true;
+            }
+
+            if (mousePosition.y < kCameraScrollBorderThickness)
+            {
+                newPosition += new Vector3(0.0f, 0.0f, -deltaTime * kCameraScrollSpeed * newPosition.y);
+                shouldMove = true;
+            }
+            else if (mousePosition.y > screenHeight - kCameraScrollBorderThickness)
+            {
+                newPosition += new Vector3(0.0f, 0.0f, deltaTime * kCameraScrollSpeed * newPosition.y);
+                shouldMove = true;
+            }
+
+            var wheel = Input.GetAxis("Mouse ScrollWheel");
+
+            if (wheel > 0.0f)
+            {
+                if (newPosition.y > kCameraZoomSpeed * kCameraMinimumHeight)
+                {
+                    newPosition.y /= kCameraZoomSpeed;
+                    shouldMove = true;
+                }
+            }
+            else if (wheel < 0.0f)
+            {
+                if (kCameraZoomSpeed * newPosition.y < kCameraMaximumHeight)
+                {
+                    newPosition.y *= kCameraZoomSpeed;
+                    shouldMove = true;
+                }
+            }
+
+            if (shouldMove)
+            {
+                m_Rigidbody.MovePosition(newPosition);
+            }
+        }
+
+        #endregion
     }
 }
