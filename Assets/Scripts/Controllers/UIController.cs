d07c8830378ca44924ec2ba14b263335994ac2b9
Sunius <suniusx@gmail.com>
2014-12-15 19:46:03 +0200

    Add UIController.

---
+++ Assets/Scripts/Controllers/UIController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TowerDefense.Controllers
+{
+    [Serializable]
+    public sealed class UIController
+    {
+        private static UIController s_Instance;
+
+        [SerializeField]
+        private Button m_StartGameButton;
+
+        [SerializeField]
+        private Text m_WaveStartingText;
+
+        [SerializeField]
+        private Text m_GameOverText;
+
+        public static Button StartGameButton { get { return s_Instance.m_StartGameButton; } }
+        public static Text WaveStartingText { get { return s_Instance.m_WaveStartingText; } }
+        public static Text GameOverText { get { return s_Instance.m_GameOverText; } }
+
+        public UIController()
+        {
+            s_Instance = this;
+        }
+
+        public void Initialize()
+        {
+            m_StartGameButton.gameObject.SetActive(false);
+            m_WaveStartingText.gameObject.SetActive(false);
+            m_GameOverText.gameObject.SetActive(false);
+        }
+    }
+}
